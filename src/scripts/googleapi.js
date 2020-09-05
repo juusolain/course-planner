a9c659a23b628fe22cd6e17295100e503c2b7960
Juuso Laine <juulain@gmail.com>
2020-09-05 21:23:27 +0300

    Hotfix for redirect URL in login

--- src/scripts/googleapi.js
+++ src/scripts/googleapi.js
@@ -50,7 +50,7 @@ class GoogleApi {
       client_secret: this.googleConfig.auth.client_secret,
       code,
       grant_type: 'authorization_code',
-      redirect_uri: 'http://localhost:8080/'
+      redirect_uri: process.env.NODE_ENV === 'production' ? 'https://courses.jusola.xyz/' : 'http://localhost:8080/'
     })
     console.log(res)
     this.setAccessToken(res.data.access_token, Date.now() + res.data.expires_in)
